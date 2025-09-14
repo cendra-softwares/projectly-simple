510e95965ce35cd0feae67a09877560a1cdcb6c3
muhammadshameelks <muhammadshameelks@gmail.com>
2025-09-15 01:45:26 +0530

    feat: uses vercel dev to dev

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rguprofepnfhducncsqh.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJndXByb2ZlcG5maGR1Y25jc3FoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NzU4MzgsImV4cCI6MjA3MzQ1MTgzOH0.3MNLE_0QdOYEGSQBuEzUTteaXHJiwYwJNH3Fb-xuaCQ'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
